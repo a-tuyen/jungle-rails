89e27c5e46730f54cd8065cb9c9e4534dc90977c
Amanda Tuyen <atuyen26@gmail.com>
2021-05-03 01:31:51 +0000

    added admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+  # http_basic_authenticate_with name: "jungle", password: "book"
   def show
   end
 end
