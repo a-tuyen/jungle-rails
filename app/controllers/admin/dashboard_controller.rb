3f9f7655edf4a450d5559f6a4bab0a2cb1b6464b
Amanda Tuyen <atuyen26@gmail.com>
2021-05-03 02:53:23 +0000

    add in totals feature to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
-  # http_basic_authenticate_with name: "jungle", password: "book"
+
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
