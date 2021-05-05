0282a588e99b4c969be8edafd1fdc0a1f2602ef4
Amanda Tuyen <atuyen26@gmail.com>
2021-05-05 10:26:13 +0000

    add in user validation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, :confirmation => true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,92 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
     
-  end
+    before :each do
+      @user = User.new(
+        first_name: 'Amanda',
+        last_name: 'Tuyen',
+        email: 'amanda@email.com',
+        password: 'amanda',
+        password_confirmation: 'amanda'
+      )
+    end
 
+    it 'is valid with all fields present and password matches password confirmation' do
+      expect(@user).to be_valid
+      expect(@user.password).to eql(@user.password_confirmation)
+    end
+
+    it 'is invalid if email is not present' do
+      @user.email = nil
+      @user.save
+
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages[0]).to eq("Email can't be blank")
+    end
+
+    it 'is invalid if first name is not present' do
+      @user.first_name = nil
+      @user.save
+
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages[0]).to eq("First name can't be blank")
+    end
+
+    it 'is invalid if last name is not present' do
+      @user.last_name = nil
+      @user.save
+
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages[0]).to eq("Last name can't be blank")
+    end
+
+    it 'is invalid if password is not present' do
+      @user.password = nil
+      @user.save
+
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages[0]).to eq("Password can't be blank")
+    end
+
+    it 'is invalid if password confirmation is not present' do
+      @user.password_confirmation = nil
+      @user.save
+
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages[0]).to eq("Password confirmation can't be blank")
+    end
+
+    it 'is invalid if password does not match password confirmation' do
+      @user.password_confirmation = 'candy'
+      @user.save
+
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages[0]).to eq("Password confirmation doesn't match Password")
+    end
+
+    it 'password length must be greater than 5 characters' do
+      @user.password = 'farm'
+      @user.password_confirmation = 'farm'
+      @user.save
+
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages[0]).to eq("Password is too short (minimum is 5 characters)")
+    end
+
+    it 'is invalid if email is not unique/in database already' do
+      new_user = User.new(
+        first_name: 'Amanda',
+        last_name: 'Tuyen',
+        email: 'AMANDA@email.com',
+        password: 'amanda',
+        password_confirmation: 'amanda'
+      )
+      new_user.save
+      @user.email = 'AMANDA@email.com'
+      @user.save
+
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages[0]).to eq("Email has already been taken")
+    end
+  end
 end
